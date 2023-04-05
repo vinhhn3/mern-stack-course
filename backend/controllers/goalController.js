4d0a015fbb3693ccd00e696388436bf91d726021
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-04-05 12:31:26 +0700

    Handle Error exception

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,8 +3,14 @@ const getGoals = (req, res) => {
 };
 
 const setGoal = (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ message: "Set goal" });
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add text field");
+  }
+
+  res.status(200).json({
+    message: "Set Goal",
+  });
 };
 const updateGoal = (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
