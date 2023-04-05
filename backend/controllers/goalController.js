322a6b0e945e1a7fe06658efec80414126b85338
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-04-05 12:16:58 +0700

    Accept body value

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,6 +3,7 @@ const getGoals = (req, res) => {
 };
 
 const setGoal = (req, res) => {
+  console.log(req.body);
   res.status(200).json({ message: "Set goal" });
 };
 const updateGoal = (req, res) => {
