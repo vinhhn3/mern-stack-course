3a6d70b05000baf5c5de8d8d460a01ef5d77cb53
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-04-06 16:44:48 +0700

    Generate JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,67 @@
-const registerUser = (req, res) => {
-  res.json({ message: "Register User" });
-};
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/user");
 
-const loginUser = (req, res) => {
-  res.json({ message: "Login User" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
 
-const getMe = (req, res) => {
+  if (!email || !password || !name) {
+    res.status(400).json({ message: "Invalid email or password or name" });
+  }
+
+  const userExists = await User.findOne({ email: email });
+  if (userExists) {
+    res.status(400).json({ message: "User already exists" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Something went wrong");
+  }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Wrong email or password");
+  }
+});
+
+const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data" });
+});
+
+const generateToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 };
 
 module.exports = { registerUser, loginUser, getMe };
