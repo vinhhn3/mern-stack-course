d616cdb19dc6f46df233f6350d2f9e7b26681f04
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-04-06 21:47:36 +0700

    Setup Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
