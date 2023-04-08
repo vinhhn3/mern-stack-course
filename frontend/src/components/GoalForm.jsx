76b3975c6e00c4e283167f8c1d76ddc7503b90f8
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-04-08 15:40:22 +0700

    Create Goal Extra Reducers

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createGoal } from "../features/goals/goalSlice";
 
 const GoalForm = () => {
-  return (
-    <div>GoalForm</div>
-  )
-}
+  const [text, setText] = useState("");
 
-export default GoalForm
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(createGoal({ text }));
+    setText("");
+  };
+  return (
+    <>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="text">Goal</label>
+            <input
+              id="text"
+              type="text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Add Goal</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default GoalForm;
